18c0635e5e2386753288df957f6c2dec6c914938
82surf <82surf@gmail.com>
2022-08-25 14:17:46 +0900

    feat: 🎸 api 연결, state 변경 시 리렌더링 기능, 초기화 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import Nodes from "./components/Nodes.js";
+import Breadcrumb from './components/Breadcrumb.js';
+import Nodes from './components/Nodes.js';
+
+import { getRootNodes } from '../lib/api.js';
 
 class App {
   constructor($app) {
+    this.$app = $app;
     this.state = {
-      isRoot: true,
+      isRoot: false,
+      path: ['노란고양이', '까만고양이'],
       nodes: [
         {
-          id: "1",
-          name: "노란고양이",
-          type: "DIRECTORY",
+          id: '1',
+          name: '노란고양이',
+          type: 'DIRECTORY',
           filePath: null,
           parent: null,
         },
         {
-          id: "3",
-          name: "까만고양이",
-          type: "DIRECTORY",
+          id: '3',
+          name: '까만고양이',
+          type: 'DIRECTORY',
           filePath: null,
           parent: null,
         },
       ],
     };
 
-    this.nodes = new Nodes($app, this.state);
+    this.initApp();
+  }
+
+  setState(nextState) {
+    this.state = nextState;
+    this.breadcrumb = new Breadcrumb(this.$app, this.state);
+    this.nodes = new Nodes(this.$app, this.state);
+  }
+
+  async initApp() {
+    const response = await getRootNodes();
+    this.state = {
+      ...this.state,
+      nodes: response,
+    };
+    this.setState(this.state);
   }
 }
 
